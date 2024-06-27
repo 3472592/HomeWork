ae4fff5cb8511e2bc56697d0ff5c6257bb9b031f
3472592 <3472592@gmail.com>
2024-06-27 05:17:15 -0400

    Add files via upload

---
+++ HW/HW/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
